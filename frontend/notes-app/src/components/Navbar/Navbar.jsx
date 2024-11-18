96237c1f18dfe7a07a4d4338d60654ec992116b1
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-11-18 22:13:11 +0530

    feat: Implement Navbar component

---
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ProfileInfo, SearchBar } from "../index";
+
+function Navbar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
+  const handleSearch = () => {
+    // navigate(`/search/${searchQuery}`);
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes-App</h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
+
+      <ProfileInfo onLogout={onLogout} />
+    </div>
+  );
+}
+
+export default Navbar;
