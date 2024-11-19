68bb1fb61f0af722b29c475986b30813b9b213cf
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-11-19 21:18:29 +0530

    Export pages from index.js

---
+++ frontend/notes-app/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import SignUp from "./SignUp/SignUp";
+import AddEditNotes from "./Home/AddEditNotes";
+
+export { Home, Login, SignUp, AddEditNotes };
