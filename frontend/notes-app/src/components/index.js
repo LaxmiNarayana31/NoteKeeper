979eecc7bcb5f7c1bf909117678850d48e2a562b
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-11-21 23:36:34 +0530

    Create barrel file for components

---
+++ frontend/notes-app/src/components/index.js
@@ -0,0 +1,8 @@
+import NoteCard from "./Cards/NoteCard";
+import ProfileInfo from "./Cards/ProfileInfo";
+import PasswordInput from "./Input/PasswordInput";
+import Navbar from "./Navbar/Navbar";
+import SearchBar from "./SearchBar/SearchBar";
+import TagInput from "./Input/TagInput";
+
+export { NoteCard, ProfileInfo, PasswordInput, Navbar, SearchBar, TagInput };
