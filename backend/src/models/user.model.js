363b54101c2dc4406887729d7bf64167e1ffad65
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-11-22 21:30:10 +0530

    User model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdOn: {
+    type: Date,
+    default: new Date().getTime(),
+  },
+});
+const User = mongoose.model("User", userSchema);
+
+export default User;
