829f0d07adde75d5c8ad72f5f410d89cce83521c
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-11-26 23:56:15 +0530

    fix: middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,15 +4,17 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const authenticateToken = asyncHandler(async (req, resp, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[1]; // Extract token from Bearer
 
   if (!token || token === "null") {
-    throw new ApiError(404, "No token provided");
+    throw new ApiError(401, "No token provided");
   }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) throw new ApiError(401, "Invalid token");
-    req.user = user;
+
+    // Ensure userId is correctly decoded
+    req.user = { userId: decoded.userId };
     next();
   });
 });
