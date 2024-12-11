541c123bb23ae8cb4b63339b1ed2ca8aa59cb69f
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-12-11 23:49:29 +0530

    export reusable components

--- frontend/notes-app/src/components/index.js
+++ frontend/notes-app/src/components/index.js
@@ -4,5 +4,14 @@ import PasswordInput from "./Input/PasswordInput";
 import Navbar from "./Navbar/Navbar";
 import SearchBar from "./SearchBar/SearchBar";
 import TagInput from "./Input/TagInput";
+import Toast from "./ToastMessage/Toast";
 
-export { NoteCard, ProfileInfo, PasswordInput, Navbar, SearchBar, TagInput };
+export {
+  NoteCard,
+  ProfileInfo,
+  PasswordInput,
+  Navbar,
+  SearchBar,
+  TagInput,
+  Toast,
+};
