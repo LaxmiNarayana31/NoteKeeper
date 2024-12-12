2f961487f66d679c52b73f7cf58cb4c3d9971dab
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-12-12 22:00:06 +0530

    Implement search functionality in Navbar

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,32 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ProfileInfo, SearchBar } from "../index";
 
-function Navbar() {
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
 
   const onLogout = () => {
+    localStorage.clear();
     navigate("/login");
   };
 
   const handleSearch = () => {
-    // navigate(`/search/${searchQuery}`);
+    if (searchQuery) {
+      onSearchNote(searchQuery);
+    }
   };
 
   const onClearSearch = () => {
     setSearchQuery("");
+    handleClearSearch("");
   };
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notes-App</h2>
+      <h2 className="text-xl font-medium text-black py-2">NoteKeeper</h2>
 
       <SearchBar
         value={searchQuery}
@@ -32,9 +37,9 @@ function Navbar() {
         onClearSearch={onClearSearch}
       />
 
-      <ProfileInfo onLogout={onLogout} />
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
   );
-}
+};
 
 export default Navbar;
